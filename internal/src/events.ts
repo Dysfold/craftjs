426ff82c317e390be1fe662fa02f8d13c9a9c566
Ap3teus <asver37@gmail.com>
2020-07-12 19:22:14 +0300

    docs: for registerEvent function

--- internal/src/events.ts
+++ internal/src/events.ts
@@ -3,6 +3,12 @@ import { EventExecutor } from "org.bukkit.plugin";
 import { Handler } from "java.util.logging";
 
 declare global {
+  /**
+   * Register Spigot event handler
+   * @param eventClass Class of the event, e.g. `org.bukkit.event.player.PlayerInteractEvent`
+   * @param callback The callback to be called when the event is triggered
+   * @param priority Priority of the event, defaults to `HIGHEST`
+   */
   function registerEvent<T extends Event>(eventClass: new (...args: any[]) => T, callback: (event: T) => void, priority?: EventPriority): () => void;
 }
 
