5b41e2e8a8bd0473a068713a5c79655a559302c0
Ap3teus <asver37@gmail.com>
2020-07-12 22:38:07 +0300

    wip: testing

---
+++ internal/dist/test/asd.test.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const zora_1 = require("zora");
+zora_1.ok(true, 'true is true');
---
+++ internal/test/asd.test.ts
@@ -0,0 +1,3 @@
+import { ok } from 'zora';
+
+ok(true, 'true is true');
