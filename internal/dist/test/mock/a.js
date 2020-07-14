f5bbe3b2fc107e5d9c7e87ad3201817734ab3795
Ap3teus <asver37@gmail.com>
2020-07-14 23:02:06 +0300

    refactor: use inline source maps

--- internal/dist/test/mock/a.js
+++ internal/dist/test/mock/a.js
@@ -5,4 +5,4 @@ exports.local = require('./b');
 exports.node1 = require('a');
 exports.node2 = require('b');
 exports.java = require('java.lang');
-//# sourceMappingURL=a.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3Rlc3QvbW9jay9hLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsdURBQXVEO0FBQzFDLFFBQUEsS0FBSyxHQUFHLE9BQU8sQ0FBQyxLQUFLLENBQUMsQ0FBQztBQUN2QixRQUFBLEtBQUssR0FBRyxPQUFPLENBQUMsR0FBRyxDQUFDLENBQUM7QUFDckIsUUFBQSxLQUFLLEdBQUcsT0FBTyxDQUFDLEdBQUcsQ0FBQyxDQUFDO0FBQ3JCLFFBQUEsSUFBSSxHQUFHLE9BQU8sQ0FBQyxXQUFXLENBQUMsQ0FBQyJ9
--- internal/dist/test/mock/b.js
+++ internal/dist/test/mock/b.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.foo = 5;
-//# sourceMappingURL=b.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3Rlc3QvbW9jay9iLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQWEsUUFBQSxHQUFHLEdBQUcsQ0FBQyxDQUFDIn0=
