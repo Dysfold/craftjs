bf9b42744e40157006a71df17fa9d2db857f9fab
Ap3teus <asver37@gmail.com>
2020-07-15 12:14:51 +0300

    fix: error in test plugin

--- plugins/test/src/index.ts
+++ plugins/test/src/index.ts
@@ -2,5 +2,5 @@ import { ServerCommandEvent } from "org.bukkit.event.server";
 import { PlayerInteractEvent } from "org.bukkit.event.player";
 
 registerEvent(PlayerInteractEvent, e => {
-  e.player.sendMessage(e.item.amount.toString());
+  e.player.sendMessage(`${e.item?.amount}`);
 });
