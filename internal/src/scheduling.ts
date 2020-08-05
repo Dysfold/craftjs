66d8abeaf3f4bde6297a74a8c1f62cb0d1ec00a5
bensku <bmi@iki.fi>
2020-08-05 20:10:45 +0300

    feat: wait function (#18)
    * feat: wait function
    fix: avoid classloading in setTimeout/Interval
    
    * fix: don't throw strings
    
    Co-authored-by: Ap3teus <63316634+Ap3teus@users.noreply.github.com>
    
    Co-authored-by: Ap3teus <63316634+Ap3teus@users.noreply.github.com>

--- internal/src/scheduling.ts
+++ internal/src/scheduling.ts
@@ -1,38 +1,18 @@
-import { Class, Object as JObject, Runnable } from 'java.lang';
-import { BukkitScheduler, BukkitTask } from 'org.bukkit.scheduler';
-import { Plugin } from 'org.bukkit.plugin';
+/**
+ * Milliseconds per server tick.
+ */
+const MILLIS_PER_TICK = 50;
 
-function getSpigotMethod(name: 'runTaskTimer' | 'runTaskLater') {
-  const long = java.lang.Long.class.getField('TYPE').get(null);
-  const c = (server.getScheduler() as any).class as Class<
-    BukkitScheduler & JObject
-  >;
-  const params = [(Plugin as any).class, (Runnable as any).class, long].concat(
-    name === 'runTaskTimer' ? [long] : [],
-  );
-  const method = c.getMethod(name, ...params) as any;
-  return (...args: any[]) => method.invoke(server.getScheduler(), ...args);
-}
-
-function getRunnable(callback: () => void) {
-  const Task = Java.extend(Runnable, {
-    run() {
-      callback();
-    },
-  });
-  return new Task();
-}
-
+/**
+ * Repeatedly executes given function.
+ * @param handler Function to execute.
+ * @param delay Delay and internal between executions (in milliseconds).
+ * @returns Task id.
+ */
 function setInterval(handler: () => void, delay: number) {
-  const runnable = getRunnable(handler);
-  const runTaskLater = getSpigotMethod('runTaskTimer');
-  const task = runTaskLater(
-    __plugin,
-    runnable,
-    delay / 50,
-    delay / 50,
-  ) as BukkitTask;
-  return task.getTaskId();
+  // Scheduler doesn't like fractional ticks
+  const ms = Math.floor(delay / MILLIS_PER_TICK);
+  return (__plugin as any).scheduleRepeating(handler, ms, ms) as number;
 }
 
 function clearInterval(tid?: number) {
@@ -42,14 +22,43 @@ function clearInterval(tid?: number) {
   server.scheduler.cancelTask(tid);
 }
 
+/**
+ * Executes a function in server thread after a delay.
+ * @param handler Function to execute.
+ * @param delay Delay in milliseconds.
+ * @returns Task id.
+ */
 function setTimeout(handler: () => void, delay: number) {
-  const runnable = getRunnable(handler);
-  const runTaskLater = getSpigotMethod('runTaskLater');
-  const task = runTaskLater(__plugin, runnable, delay / 50) as BukkitTask;
-  return task.getTaskId();
+  return (__plugin as any).scheduleOnce(handler, Math.floor(delay / MILLIS_PER_TICK)) as number;
 }
 
+// Override JS standard library timers
 global.setInterval = setInterval as any;
 global.setTimeout = setTimeout as any;
 global.clearInterval = clearInterval;
 global.clearTimeout = clearInterval;
+
+
+// See global.wait for docs
+function wait(delay: number = 0, unit: TimeUnit = TimeUnit.TICKS): Promise<void> {
+  let ms: number;
+  switch (unit) {
+    case TimeUnit.MILLIS:
+      ms = delay;
+      break;
+    case TimeUnit.TICKS:
+      ms = delay * MILLIS_PER_TICK;
+      break;
+    case TimeUnit.SECONDS:
+      ms = delay * 1000;
+      break;
+    case TimeUnit.MINUTES:
+      ms = delay * 1000 * 60
+      break;
+    default:
+      throw new Error('unknown time unit');
+  }
+  return new Promise(resolve => setTimeout(() => resolve(), ms));
+}
+
+global.wait = wait;
