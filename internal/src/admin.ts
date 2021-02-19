acad67dd5e3cb603335270d4fc8fd2950a052dfb
bensku <bmi@iki.fi>
2021-02-20 00:50:05 +0200

    feat: sync commands after reload (fixes tab completion)

--- internal/src/admin.ts
+++ internal/src/admin.ts
@@ -1,3 +1,4 @@
+import { Bukkit } from 'org.bukkit';
 import { runTests } from './test-runner';
 
 registerCommand(
@@ -14,6 +15,11 @@ registerCommand(
         sender.sendMessage(`... failed! See console for more details.`);
       } else {
         sender.sendMessage('... done!');
+        try {
+          (Bukkit.server as any).syncCommands();
+        } catch {
+          // Ignore, syncCommands is unsupported CraftBukkit API
+        }
       }
     } else if (action == 'test') {
       const plugin = args[1];
