df298c4ece124488038b5075ff63a0e8f18cfe75
bensku <bmi@iki.fi>
2021-02-25 19:26:09 +0200

    chore: improve build script error handling

--- build.sh
+++ build.sh
@@ -2,6 +2,17 @@
 # Builds CraftJS Java and JS components
 set -euo pipefail
 
+root_dir=$PWD
+function cleanup {
+    echo "Restoring original node_modules..."
+    cd $root_dir
+    rm -rf core/node_modules
+    mv core_modules.temp core/node_modules
+    rm -rf internal/node_modules
+    mv internal_modules.temp internal/node_modules
+}
+trap cleanup EXIT
+
 echo "Compiling core TS to JS..."
 npm run tsc --prefix=core
 npm run tsc --prefix=internal
@@ -18,11 +29,5 @@ echo "Compiling and generating jar..."
 cd ../java
 mvn clean package
 cd ..
-
-echo "Restoring original node_modules..."
-rm -rf core/node_modules
-mv core_modules.temp core/node_modules
-rm -rf internal/node_modules
-mv internal_modules.temp internal/node_modules
 
 echo "DONE! craftjs.jar built at java/target"
