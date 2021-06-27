d8d3460ffbe75531b14ec7bb1c824361ca558c8a
bensku <bmi@iki.fi>
2021-06-27 15:32:21 +0300

    chore: clean up unused files that caused dependabot alerts

--- plugins/test/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ServerCommandEvent } from "org.bukkit.event.server";
-import { PlayerInteractEvent } from "org.bukkit.event.player";
-
-registerEvent(PlayerInteractEvent, e => {
-  e.player.sendMessage(`${e.item?.amount}`);
-});
